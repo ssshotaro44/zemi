e95690a62632b3a80ccd1f3bf64da8f29ec806fe
shotaro-fim <shotaro_miyamoto@future-i-mark.co.jp>
2021-07-11 14:48:13 +0900

    リスト7.10まで実装

--- practice_tf.py
+++ practice_tf.py
@@ -1,6 +1,11 @@
 import os
 import cv2
+import keras.optimizers as optimizers
+from keras.layers import Input, Conv2D
+from keras.models import Model
 import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.python.keras import backend as K
 
 
 def load_images(inputpath, imagesize, type_color):
@@ -36,7 +41,7 @@ def load_images(inputpath, imagesize, type_color):
                 
             imglist.append(testimage)
         imgsdata = np.asarray(imglist, dtype=np.float32)
-        
+
         return imgsdata, sorted(files)
 
 
@@ -83,3 +88,68 @@ imagenoise_train /= 255.0
 image_train /= 255.0
 imagenoise_test /= 255.0
 image_test /= 255.0
+
+
+# リスト7.7
+CHANNEL = 1
+
+
+def network_denoisetestnet():
+    input_img = Input(shape=(IMAGE_SIZE, IMAGE_SIZE, CHANNEL))
+    
+    x = Conv2D(64, kernel_size=3, activation='relu', padding='same')(input_img)
+    x = Conv2D(64, kernel_size=3, activation='relu', padding='same')(x)
+    x = Conv2D(1, kernel_size=3, activation='linear', padding='same')(x)
+    
+    model = Model(input_img, x)
+    
+    return model
+
+
+model = network_denoisetestnet()
+print(model.summary())
+
+
+# リスト7.8
+def psnr(y_true, y_pred):
+    return -10 * K.log(K.mean(K.flatten((y_true - y_pred)) ** 2)) / np.log(10)
+
+
+# traningの設定
+adam = optimizers.Adam(lr=1e-3)
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=[psnr])
+
+# traing実行
+training = model.fit(imagenoise_train, image_train, epochs=50, batch_size=10, shuffle=True, validation_data=(imagenoise_test, image_test), verbose=1)
+
+
+# リスト7.9
+# 学習履歴グラフ表示
+# val_psnr&val_loss:検証用データセットに対する値, psnr&loss:学習用データセットに対する値
+def plot_history(history):
+    plt.plot(history.history['psnr'])
+    plt.plot(history.history['val_psnr'])
+    plt.title('model accuracy')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.legend(['psnr', 'val_psnr'], loc='lower right')
+    plt.show()
+    
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(['loss', 'val_loss'], loc='lower right')
+    plt.show()
+
+
+plot_history(training)
+
+
+# リスト7.10
+# networkの性能を確認するためにノイズ画像からノイズを取り除いた画像を出力
+results = model.predixt(imagenoise_test, verbose=1)
+
+results *= 255.0
+save_images("./result/", imagenoise_test_filenames, results)
